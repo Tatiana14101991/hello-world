558a7ad89a2eac5fbe419f1acb1a9853cae35b33
Tatiana14101991 <vagner432932326@mail.ru>
2019-06-24 20:28:05 +0300

    2commit

---
+++ src/Strings.java
@@ -0,0 +1,16 @@
+
+
+
+
+public class Strings {
+    public static void main(String[] args){
+      int x=5;
+String s="spitz";
+String space="";
+String name="Deny";
+System.out.println(s+space+name);
+System.out.println("spitz"+" "+"Deny");
+System.out.println("My number is "+x);}
+
+
+}
---
+++ src/Veriablles.java
@@ -0,0 +1,17 @@
+public class Veriablles {
+    public static void main(String[] args){
+        int myInt;
+        myInt=3;
+        short myShort=31999;
+        long myLong=32333;
+        double myDoule=333.33;
+        float myFloat=333.33f;
+        char c='a';
+        boolean b=true;
+       byte myByte=100; //-128-127
+
+        System.out.println(myInt);
+
+    }
+
+}
